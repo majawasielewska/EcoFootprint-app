ff317289ee3ecbc086adc9a00eec7de6d7aa5aac
Maja Wasielewska <101111842+majawasielewska@users.noreply.github.com>
2024-06-12 01:47:17 +0200

    Update models.py

--- django_website/carbon_footprint_website/models.py
+++ django_website/carbon_footprint_website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class UserFootprint(models.Model):
+    user_name = models.CharField(max_length=100)
+    energy_consumption = models.FloatField(help_text="Energy consumption in kWh")
+    meat_consumption = models.FloatField(help_text="Amount of meat consumed in kg per week")
+    travel_distance = models.FloatField(help_text="Distance traveled by car in km per week")
+    waste_production = models.FloatField(help_text="Waste produced in kg per week")
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user_name
+
