378ced4a5331c648d5271a9582fc81c9c02756cd
Maja Wasielewska <101111842+majawasielewska@users.noreply.github.com>
2024-06-12 01:46:32 +0200

    Update views.py

--- django_website/carbon_footprint_website/views.py
+++ django_website/carbon_footprint_website/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserFootprintForm
+from .models import UserFootprint
 
+def calculate_footprint(request):
+    if request.method == 'POST':
+        form = UserFootprintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('result')
+    else:
+        form = UserFootprintForm()
+    return render(request, 'ecofootprint/calculate.html', {'form': form})
 
-def index(request):
-    return render(request, "index.html")
+def result(request):
+    latest_entry = UserFootprint.objects.latest('date_created')
+    total_footprint = (
+        latest_entry.energy_consumption * 0.233 +
+        latest_entry.meat_consumption * 7.2 +
+        latest_entry.travel_distance * 0.21 +
+        latest_entry.waste_production * 0.5
+    )
+    return render(request, 'ecofootprint/result.html', {'entry': latest_entry, 'total_footprint': total_footprint})
+
