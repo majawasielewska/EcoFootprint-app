b9dda83e93eccc08f78d4a7ca33907bc058d2bb7
tomczyk <michal12tomczyk@gmail.com>
2024-06-14 02:55:57 +0200

    add percentiles calculation

--- django_website/carbon_footprint_website/views.py
+++ django_website/carbon_footprint_website/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserFootprintForm
-from .models import UserFootprint
+from .models import UserFootprint, UserFootprintAggregation
 from django.contrib.auth.forms import UserCreationForm
+from scipy import stats
 
 
 def home(request):
@@ -36,10 +37,10 @@ def calculate_footprint(request):
             footprint.travel_distance_footprint = footprint.travel_distance * 0.21
             footprint.waste_production_footprint = footprint.waste_production * 0.5
             footprint.total_footprint = (
-                footprint.energy_consumption_footprint +
-                footprint.meat_consumption_footprint +
-                footprint.travel_distance_footprint +
-                footprint.waste_production_footprint
+                    footprint.energy_consumption_footprint +
+                    footprint.meat_consumption_footprint +
+                    footprint.travel_distance_footprint +
+                    footprint.waste_production_footprint
             )
             footprint.save()
             return redirect('result')
@@ -68,6 +69,38 @@ def result(request):
 
 def account(request):
     user = request.user
-    user_data = UserFootprint.objects.filter(user=request.user)
-    print(user_data)
-    return render(request, 'ecofootprint/user.html', {'user': user})
+    user_data = UserFootprint.objects.filter(user=request.user).order_by('-date_created')[:5]
+    try:
+        data_agg = UserFootprintAggregation.objects
+        user_data_agg = data_agg.filter(user=request.user)
+        percentiles = calculate_percentiles(user_data_agg, data_agg)
+    except Exception:
+        user_data_agg = {}
+        percentiles = {}
+    return render(request, 'ecofootprint/user.html', {'user': user, 'user_data': user_data,
+                                                      'user_data_agg': user_data_agg, 'percentiles': percentiles})
+
+
+def calculate_percentiles(user_data_agg, data_agg):
+    percentiles = {}
+
+    user_item = user_data_agg.latest('week')
+    overall_week_data = data_agg.filter(week=user_item.week)
+    # Calculate percentiles for each value in user_week_data
+    values = overall_week_data.values_list('mean_energy_consumption_footprint', flat=True)
+    percentile_energy = stats.percentileofscore(values, user_item.mean_energy_consumption_footprint)
+    percentiles["energy"] = percentile_energy
+    values = overall_week_data.values_list('mean_meat_consumption_footprint', flat=True)
+    percentile_meat = stats.percentileofscore(values, user_item.mean_meat_consumption_footprint)
+    percentiles["meat"] = percentile_meat
+    values = overall_week_data.values_list('mean_travel_distance_footprint', flat=True)
+    percentile_travel = stats.percentileofscore(values, user_item.mean_travel_distance_footprint)
+    percentiles["travel"] = percentile_travel
+    values = overall_week_data.values_list('mean_waste_production_footprint', flat=True)
+    percentile_waste = stats.percentileofscore(values, user_item.mean_waste_production_footprint)
+    percentiles["waste"] = percentile_waste
+    values = overall_week_data.values_list('mean_total_footprint', flat=True)
+    percentile_total = stats.percentileofscore(values, user_item.mean_total_footprint)
+    percentiles["total"] = percentile_total
+
+    return percentiles
